30717c4ed88649bba822aedb681e9bf7a7cf1ba2
onedim <o.nedim@gmail.com>
2014-12-22 10:28:14 +0100

    Update Circle.java

--- src/Circle.java
+++ src/Circle.java
@@ -1,4 +1,31 @@
+import java.awt.Color;
+import java.awt.Graphics;
 
 public class Circle extends Geometry {
+  
+  private int radius;
+  
+  /**
+	 * creates an arch
+	 * @param positionX int first coordinate
+	 * @param positionY int second coordinate
+	 * @param color Color colore of the circle
+	 * @param radius int radius of the circle
+	  */
+	public Circle(int positionX, int positionY, Color color, int radius ) {
+		super(positionX, positionY, color);
+		this.radius = radius;
+		
+		
+	}
+	public void draw(Graphics g, int frame) {
+		g.setColor(super.getColor());
+		g.drawCircle(super.getPositionX(), super.getPositionY(), this.radius);
+	}
+	
+	public void move(int frame) {
+		return;
+	}
+	
 
 }
